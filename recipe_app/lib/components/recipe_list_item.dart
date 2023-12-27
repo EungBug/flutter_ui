627741ff4de7ce8e2fec040a2856faeef1ad0539
EungBug <eungb14@gmail.com>
2023-12-27 22:08:55 +0900

    💄 page 기본 ui 구조 작업

---
+++ recipe_app/lib/components/recipe_list_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem({
+    super.key,
+    required this.imageName,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ recipe_app/lib/pages/recipe_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app/components/recipe_list_item.dart';
+import 'package:recipe_app/components/recipe_menu.dart';
+import 'package:recipe_app/components/recipe_title.dart';
+
+class RecipePage extends StatelessWidget {
+  const RecipePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _buildRecipeAppBar(),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            RecipeTitle(),
+            RecipeMenu(),
+            RecipeListItem(
+              imageName: "coffe",
+              title: "Made Coffee",
+            ),
+            RecipeListItem(
+              imageName: "burger",
+              title: "Made Burger",
+            ),
+            RecipeListItem(
+              imageName: "pizza",
+              title: "Made Pizza",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+AppBar _buildRecipeAppBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 1.0,
+    surfaceTintColor: Colors.white,
+    shadowColor: Colors.black,
+    actions: const [
+      Icon(
+        CupertinoIcons.search,
+        color: Colors.black,
+      ),
+      SizedBox(
+        width: 15,
+      ),
+      Icon(
+        CupertinoIcons.heart,
+        color: Colors.redAccent,
+      ),
+      SizedBox(
+        width: 15,
+      ),
+    ],
+  );
+}
