5e8daf9eadfb68942ac30f5766d0a748440a5957
EungBug <eungb14@gmail.com>
2024-01-09 21:29:19 +0900

    📝 기본 구조 및 에셋 추가

---
+++ cart_app/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:cart_app/constants.dart';
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kPrimaryColor,
+  );
+}
