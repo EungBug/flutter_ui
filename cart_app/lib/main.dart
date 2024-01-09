441daf8983390dda95d0caf36534baf56683a444
EungBug <eungb14@gmail.com>
2024-01-09 21:44:16 +0900

    💄 앱 기본 뼈대 구성

--- cart_app/lib/main.dart
+++ cart_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:cart_app/components/shoppingcart_detail.dart';
+import 'package:cart_app/components/shoppingcart_header.dart';
+import 'package:cart_app/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +12,30 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: theme(),
+      home: const ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: const Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar();
   }
 }
